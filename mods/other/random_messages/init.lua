ea827924bd599f521e18d0ce407f681633b1010f
rubenwardy <rw@rubenwardy.com>
2019-03-05 15:09:17 +0000

    Replace random messages

--- mods/other/random_messages/init.lua
+++ mods/other/random_messages/init.lua
@@ -47,25 +47,7 @@ end
 
 function random_messages.read_messages()
 	random_messages.messages = {
-		"To talk to only your team, start your messages with /t. For example, /t Hello team!",
-		"Eat apples to restore health quickly.",
-		"Steel swords do more damage than guns, but you need to be up close.",
-		"Gain more score by killing more than you die, or by capturing the flag.",
-		"You gain more score the better the opponent you defeat.",
-		"Find weapons in chests or mine and use furnaces to make stronger swords.",
-		"Players are immune to attack for 15 seconds after they respawn.",
-		"Access the pro section of the chest by achieving a 1k+ score and killing 3 people for every 2 deaths.",
-		"Use team doors (steel) to stop the enemy walking into your base.",
-		"Sprint by pressing the fast key (E) when you have stamina.",
-		"Like CTF? Give feedback using /report, and consider donating at rubenwardy.com/donate",
-		"Map makers needed! Visit ctf.rubenwardy.com to get involved.",
-		"Using limited resources for building structures that don't strengthen your base's defences is discouraged.",
-		"To report misbehaving players to moderators, please use /report <name> <action>",
-		"Swearing, trolling and being rude will not be tolerated and strict action will be taken.",
-		"Trapping team mates on purpose is strictly against the rules and you will be kicked immediately.",
-		"Help your team claim victory by storing extra weapons in the team chest, and never taking more than you need.",
-		"Excessive spawn-killing is a direct violation of the rules - appropriate punishments will be given.",
-		"Use /r to check your score and rank, and /rankings to see the league tables."
+		"Please update to Minetest 5.0 to get new features such as slippery nodes and traps!\nVisit minetest.net or search for Minetest on the Play Store"
 	}
 end
 
