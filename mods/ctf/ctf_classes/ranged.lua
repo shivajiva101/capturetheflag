cadf51f9cacb0a233cb11414227f79b1b538aa59
rubenwardy <rw@rubenwardy.com>
2019-03-22 04:22:42 +0000

    Make grapples shooter-only

--- mods/ctf/ctf_classes/ranged.lua
+++ mods/ctf/ctf_classes/ranged.lua
@@ -34,3 +34,25 @@ shooter.get_weapon_spec = function(_, user, name)
 	spec.tool_caps.full_punch_interval = spec.tool_caps.full_punch_interval * 0.8
 	return spec
 end
+
+
+local function check_grapple(itemname)
+	local def = minetest.registered_items[itemname]
+	local old_func = def.on_use
+	minetest.override_item(itemname, {
+		description = def.description .. "\nCan only be used by Shooters",
+		on_use = function(itemstack, user, ...)
+			if ctf_classes.get(user).name ~= "shooter" then
+				minetest.chat_send_player(user:get_player_name(),
+					"Only Shooters are skilled enough for grapples! Change your class at spawn")
+
+				return itemstack
+			end
+
+			return old_func(itemstack, user, ...)
+		end,
+	})
+end
+
+check_grapple("shooter:grapple_gun_loaded")
+check_grapple("shooter:grapple_gun")
