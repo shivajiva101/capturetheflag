28c49a7e7f543bd41914d870ba72b0f459832d8f
rubenwardy <rw@rubenwardy.com>
2019-03-22 03:18:45 +0000

    Add hpregen for medics

--- mods/ctf/ctf_classes/init.lua
+++ mods/ctf/ctf_classes/init.lua
@@ -5,6 +5,7 @@ ctf_classes = {
 
 dofile(minetest.get_modpath("ctf_classes") .. "/api.lua")
 dofile(minetest.get_modpath("ctf_classes") .. "/gui.lua")
+dofile(minetest.get_modpath("ctf_classes") .. "/regen.lua")
 
 ctf_classes.register("knight", {
 	description = "Knight",
